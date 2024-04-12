5547d54a0eee74fee75c1eb6fb18ca1e6aa77f45
atulanand1811 <61856564+atulanand1811@users.noreply.github.com>
2024-04-12 20:38:57 +0530

    removed db

--- index.js
+++ index.js
@@ -1,106 +1,106 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', true)
+// mongoose.set('strictQuery', true)
 
-mongoose.connect('mongodb+srv://atulanand1811:my%40portfolio%40db%402024@pf-db.2l7lwqb.mongodb.net/?retryWrites=true&w=majority&appName=pf-db', {
-	dbName: 'portfolio-db',
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-}, err => err ? console.log(err) :
-	console.log('Connected to portfolio-db database'));
+// mongoose.connect('mongodb+srv://atulanand1811:my%40portfolio%40db%402024@pf-db.2l7lwqb.mongodb.net/?retryWrites=true&w=majority&appName=pf-db', {
+// 	dbName: 'portfolio-db',
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true
+// }, err => err ? console.log(err) :
+// 	console.log('Connected to portfolio-db database'));
 
-// Schema for users of app
-const UserDetails = new mongoose.Schema({
-	portfolioId: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	image: {
-		type: String,
-		required: true,
-	},
-	userName: {
-		type: String,
-		required: true,
-	},
-	profession: {
-		type: String,
-		required: true,
-	},
-	summary: {
-		type: String,
-		required: true,
-	},
-	aboutMe: {
-		type: String,
-		required: true,
-	},
-	skills: {
-		type: String,
-		required: true,
-	},
-	linkedIn: {
-		type: String,
-		required: true,
-	},
-	github: {
-		type: String,
-		required: true,
-	},
-	emailId: {
-		type: String,
-		required: true,
-	},
-	works: [{
-		name: {
-			type: String,
-			required: true
-		},
-		description: {
-			type: String,
-			required: true
-		},
-		techstack: {
-			type: String,
-			required: true
-		},
-		url: {
-			type: String,
-			required: false
-		}
-	}],
-	experience: [{
-		organization: {
-			type: String,
-			required: true
-		},
-		timeToFrom: {
-			type: String,
-			required: true
-		},
-		projects: [{
-			name: {
-				type: String,
-				required: false
-			},
-			role: {
-				type: String,
-				required: true
-			},
-			responsibility: {
-				type: String,
-				required: true
-			},
-			techstack: {
-				type: String,
-				required: true
-			}
-		}]
-	}]
-});
-const User = mongoose.model('userdetails', UserDetails);
-User.createIndexes();
+// // Schema for users of app
+// const UserDetails = new mongoose.Schema({
+// 	portfolioId: {
+// 		type: String,
+// 		required: true,
+// 		unique: true
+// 	},
+// 	image: {
+// 		type: String,
+// 		required: true,
+// 	},
+// 	userName: {
+// 		type: String,
+// 		required: true,
+// 	},
+// 	profession: {
+// 		type: String,
+// 		required: true,
+// 	},
+// 	summary: {
+// 		type: String,
+// 		required: true,
+// 	},
+// 	aboutMe: {
+// 		type: String,
+// 		required: true,
+// 	},
+// 	skills: {
+// 		type: String,
+// 		required: true,
+// 	},
+// 	linkedIn: {
+// 		type: String,
+// 		required: true,
+// 	},
+// 	github: {
+// 		type: String,
+// 		required: true,
+// 	},
+// 	emailId: {
+// 		type: String,
+// 		required: true,
+// 	},
+// 	works: [{
+// 		name: {
+// 			type: String,
+// 			required: true
+// 		},
+// 		description: {
+// 			type: String,
+// 			required: true
+// 		},
+// 		techstack: {
+// 			type: String,
+// 			required: true
+// 		},
+// 		url: {
+// 			type: String,
+// 			required: false
+// 		}
+// 	}],
+// 	experience: [{
+// 		organization: {
+// 			type: String,
+// 			required: true
+// 		},
+// 		timeToFrom: {
+// 			type: String,
+// 			required: true
+// 		},
+// 		projects: [{
+// 			name: {
+// 				type: String,
+// 				required: false
+// 			},
+// 			role: {
+// 				type: String,
+// 				required: true
+// 			},
+// 			responsibility: {
+// 				type: String,
+// 				required: true
+// 			},
+// 			techstack: {
+// 				type: String,
+// 				required: true
+// 			}
+// 		}]
+// 	}]
+// });
+// const User = mongoose.model('userdetails', UserDetails);
+// User.createIndexes();
 
 // For backend and express
 const express = require('express');
@@ -118,45 +118,45 @@ app.get("/", (req, resp) => {
 	// backend working properly
 });
 
-app.post("/register", async (req, resp) => {
-	try {
-		const user = new User(req.body);
-		console.log(user.userName);
-		let name = user.userName.split(' ').join('-').toLowerCase();
-		let characters = "abcdefghiklmnopqrstuvwxyz123467890";
-		let lenString = 14;
-		let randomstring = '';
-		for (let i = 0; i < lenString; i++) {
-			let rnum = Math.floor(Math.random() * characters.length);
-			randomstring += characters.substring(rnum, rnum + 1);
-		}
-		let portfolioId = name + "-" + randomstring;
-		user.portfolioId = portfolioId;
-		let result = await user.save();
-		result = result.toObject();
-		if (result) {
-			let url = `localhost:3000/portfolio/${result.portfolioId}`;
-			resp.send(url);
-			console.log(result);
-		} else {
-			console.log("User already register");
-		}
+// app.post("/register", async (req, resp) => {
+// 	try {
+// 		const user = new User(req.body);
+// 		console.log(user.userName);
+// 		let name = user.userName.split(' ').join('-').toLowerCase();
+// 		let characters = "abcdefghiklmnopqrstuvwxyz123467890";
+// 		let lenString = 14;
+// 		let randomstring = '';
+// 		for (let i = 0; i < lenString; i++) {
+// 			let rnum = Math.floor(Math.random() * characters.length);
+// 			randomstring += characters.substring(rnum, rnum + 1);
+// 		}
+// 		let portfolioId = name + "-" + randomstring;
+// 		user.portfolioId = portfolioId;
+// 		let result = await user.save();
+// 		result = result.toObject();
+// 		if (result) {
+// 			let url = `localhost:3000/portfolio/${result.portfolioId}`;
+// 			resp.send(url);
+// 			console.log(result);
+// 		} else {
+// 			console.log("User already register");
+// 		}
 
-	} catch (e) {
-		resp.send("Something Went Wrong" + e.message);
-	}
-});
+// 	} catch (e) {
+// 		resp.send("Something Went Wrong" + e.message);
+// 	}
+// });
 
-app.get('/user/:id', async (req, resp) => {
+// app.get('/user/:id', async (req, resp) => {
 
-	let portfolioId = req.params.id;
-	console.log(portfolioId);
-	User.find({ portfolioId: portfolioId }, function (err, user) {
-		if (err) {
-			resp.send(err);
-		}
-		resp.json(user);
-	});
+// 	let portfolioId = req.params.id;
+// 	console.log(portfolioId);
+// 	User.find({ portfolioId: portfolioId }, function (err, user) {
+// 		if (err) {
+// 			resp.send(err);
+// 		}
+// 		resp.json(user);
+// 	});
 
-});
+// });
 app.listen(5000);
